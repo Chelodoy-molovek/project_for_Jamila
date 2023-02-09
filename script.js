eaa35c9e78de472e3b7efbfdfe1eb185b8508833
Islam <alaev095@bk.ru>
2023-02-09 20:22:58 +0300

    addImg

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+import {database1} from './database.js'
 const container = document.querySelector(".container");
+console.log(database1)
+
+//////////////////////////////////////////////////////////////////////////// (create html blocks)
+let count = 0;
+
 for (let i = 0; i < 6; i++) {
   const newDiv = document.createElement("div");
   container.append(newDiv);
   newDiv.className = "row_container";
   for (let q = 0; q < 5; q++) {
+    
     const newDivChild = document.createElement("div");
+    const newImg = document.createElement('img');
+    newImg.src=`${database1[count].img}`
+    newImg.className = "imgItems"
+    newDivChild.append(newImg);
     newDiv.append(newDivChild);
-    newDivChild.className = "element";
+    newDivChild.className = `element id${count}`;
+    count = count + 1;
   }
 }
+
+
+////////////////////////////////////////////////////////////////////////////   popup
+
+let allelements = document.querySelectorAll(".element");
+let popupOpen = document.querySelector(".popup");
+allelements.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    e.preventDefault()
+    popupOpen.style.display = "block";
+  });
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////
